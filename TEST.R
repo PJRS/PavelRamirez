49c833a368c4b18dd084bc0372110751136bd28e
Pavel <pavelramirez@outlook.com>
2019-11-23 15:37:28 +0100

    test

---
+++ TEST.R
@@ -0,0 +1,12 @@
+---
+  title: "Castor Canadensis"
+author: "TEST"
+date: "29/10/2019"
+output: 
+  html_document:
+  toc: true
+toc_float:
+  collapsed: false
+smooth_scroll: false
+theme: united
+highlight: tango
